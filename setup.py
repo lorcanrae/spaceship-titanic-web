fc659dd9113ac4b3671df99b00cc197d05d056fa
Lorcan Rae <lorcanrae@outlook.com>
2022-07-26 16:01:34 +0100

    removing un-needed

--- setup.py
+++ setup.py
@@ -7,11 +7,7 @@ requirements = [x.strip() for x in content if 'git+' not in x]
 
 setup(name='spaceship-titanic-web',
       version="1.0",
-      description="Project Description",
+      description="spaceship-titanic streamlit app",
       packages=find_packages(),
       install_requires=requirements,
-      test_suite='tests',
-      # include_package_data: to install data from MANIFEST.in
-      include_package_data=True,
-      scripts=['scripts/spaceship-titanic-web-run'],
-      zip_safe=False)
+      include_package_data=True)
