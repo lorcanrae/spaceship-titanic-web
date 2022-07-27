ec8f503548af0ff58a73cefb4191d0b6928695e7
Lorcan Rae <lorcanrae@outlook.com>
2022-07-27 17:26:18 +0100

    science

--- pages/2_🔌_API, Model and Data.py
+++ pages/2_🔌_API, Model and Data.py
@@ -39,23 +39,24 @@ with sbcol3:
 api_url = 'https://spaceship-titanic-api-zby5e6zv3q-ew.a.run.app'
 
 st.markdown('### API:')
-st.markdown("""This API was created by containerising a python package with Docker, pushing \
-            the package to GCP Container Registry, and exposing the API using \
-            GCP Cloud Run. FastAPI and Uvicorn where used to build the api.\
+st.markdown("""This API was created by containerising a python package with Docker, pushing
+            the package to GCP Container Registry, and exposing the API using
+            GCP Cloud Run. FastAPI and Uvicorn where used to build the api.
             \nThe API base endpoint can be found [here]({api_url}). Prediction can be accessed at the /predict endpoint.""")
 st.markdown(f"""Generally, people who are older and spend more on Room Service, Food, Shopping, Spa and VR are not transported (10,000+ per category).""")
 
 st.markdown('### Modelling:')
-st.markdown("""An ensemble voting classifier composed of a Suport Vector Classifier with a Linear kernel, \
+st.markdown("""An ensemble voting classifier composed of a Suport Vector Classifier with a Linear kernel,
 KNN Classifier, and Gradient Boosted Random Forest was used. The model was positioned at the end of a
-pipeline that transformed, imputed, encoded, and scaled the data. Hyperparameters \
+pipeline that transformed, imputed, encoded, and scaled the data. Hyperparameters
 where optimized using a gridsearch.
 The model has an accuracy of 80.0009% on the test data set.""")
 st.markdown('This model was trained in the cloud using GCP AI Platform - for my own practice.')
 
 st.markdown('### Data:')
-st.markdown("""The data used for the model is from the Spaceship Titanic dataset available on kaggle. \
-Refer to kaggles [Spaceship Titanic competition](https://www.kaggle.com/competitions/spaceship-titanic) for specifics. \
-I have performed an Exploratory Data Analysis of this data set, including Univariate and Bivariate anayslse, \
-available on my [github](https://github.com/lorcanrae/spaceship-titanic/tree/master/notebooks) or my [kaggle profile](https://www.kaggle.com/code/lorcansamuel/spaceship-titanic-eda-ensemble-using-pipes/notebook).
+st.markdown("""The data used for the model is from the Spaceship Titanic dataset available on kaggle.
+Refer to kaggles [Spaceship Titanic competition](https://www.kaggle.com/competitions/spaceship-titanic) for specifics.
+I have performed an Exploratory Data Analysis of this data set, including Univariate and Bivariate anayslse,
+available on my [github](https://github.com/lorcanrae/spaceship-titanic/tree/master/notebooks) or my
+[kaggle profile](https://www.kaggle.com/code/lorcansamuel/spaceship-titanic-eda-ensemble-using-pipes/notebook).
             """)
